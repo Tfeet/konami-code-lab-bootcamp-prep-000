5ae79f8d632a57ec84a6b0ba78270b9b0cf0d09c
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-18 20:23:01 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,21 +11,3 @@ const codes = [
   "a"
 ];
 
-   let index = 0;
-}	    document.body.addEventListener("keydown", function(e) {
-        // function onKeyDownHandler(e) {
-        const key = e.key;
-
-        if (key === codes[index]) {
-            index++;
-            if (index === codes.length) {
-                alert("Hurray!");
-
-                index = 0;
-            }
-        } else {
-            index = 0;
-        }
-        // }
-    })
-} 
